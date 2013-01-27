72ff784ef0415003cb87d3404b654759fb392b64
tallbryan <b_d_williams@yahoo.com>
2013-01-27 15:57:19 -0800

    add show action and view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,18 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
+  
+  def create
+    @project = Project.new(params[:project]) 
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      # nothing, yet
+    end
+  end
+  
+  def show
+    @project=Project.find(params[:id])
+  end
 end
