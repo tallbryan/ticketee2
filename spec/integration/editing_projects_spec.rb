48196c633bdae64986738983e3ceafe40f90cd1a
tallbryan <b_d_williams@yahoo.com>
2013-02-02 15:24:51 -0800

    add updating projects functionality

--- spec/integration/editing_projects_spec.rb
+++ spec/integration/editing_projects_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
 feature "Editing Projects" do
-  scenario "Updating a project" do
+  before do
     Factory(:project, :name => "TextMate 2")
     visit "/"
-    click_link "Textmate 2"
-    click_link "EditProject"
+    click_link "TextMate 2"
+    click_link "Edit Project"
+  end
+  
+  scenario "Updating a project" do
     fill_in "Name", :with => "TextMate 2 beta"
     click_button "Update Project"
     page.should have_content("Project has been updated.")
+  end
+  
+  scenario "Updating a project with invalid attributes is bad" do
+    fill_in "Name", :with => ""
+    click_button "Update Project"
+    page.should have_content("Project has not been updated.")
   end
 end
