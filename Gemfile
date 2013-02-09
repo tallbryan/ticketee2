d3f0ba3db02913d5432a5d72d8bd25a3f00c9230
tallbryan <b_d_williams@yahoo.com>
2013-02-09 12:38:00 -0800

    Add feature to ensure Devise signup is always working

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ end
 group :test do
   gem 'capybara', '1.1.2'
   gem 'factory_girl', '2.6.4'
+  gem 'devise', '2.1.0.rc'
 end
 
 # To use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Ticketee2::Application.routes.draw do
+  devise_for :users
+
   root :to => "projects#index"
   resources :projects do
     resources :tickets
   end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
