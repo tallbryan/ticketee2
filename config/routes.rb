36c1877eae183c859646e72d2256db88ea0ff1ee
tallbryan <b_d_williams@yahoo.com>
2013-02-17 11:07:06 -0800

    Add admin namespaced users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Ticketee2::Application.routes.draw do
+
   devise_for :users
 
   root :to => "projects#index"
   resources :projects do
     resources :tickets
+  end
+  
+  namespace :admin do
+    root :to => "base#index"
+    resources :users
   end
   
   # The priority is based upon order of creation:
