b533a2219a247d1152d79b64e67ca44c4f05c8ed
tallbryan <b_d_williams@yahoo.com>
2013-02-16 20:55:04 -0800

    Restrict access to project actions to admins only

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ require 'email_spec'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.include Devise::TestHelpers, :type => :controller
   config.before do
     ActionMailer::Base.deliveries.clear
   end
