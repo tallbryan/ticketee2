f7e72849c9dcbf68b3e9df1581ebd2ffe03c0469
tallbryan <b_d_williams@yahoo.com>
2013-02-18 22:16:58 -0800

    Add ability to update users

--- spec/support/user_factory.rb
+++ spec/support/user_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    email "ticketee@email.com"
+    sequence(:email) { |n| "user#{n}@ticketee.com" }
     password "password"
     password_confirmation "password"
 
